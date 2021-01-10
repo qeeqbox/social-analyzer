8b312a171c8891ef713f82c72f20a486284f5edc
G <52905881+giga-a@users.noreply.github.com>
2021-01-09 16:18:27 -0800

    [auto-b] more customization to the python CLI version - ref f39f7001

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 #
 [![Generic badge](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/qeeqbox/social-analyzer/main/info&label=version&query=$.version&colorB=blue&style=flat-square)](https://github.com/qeeqbox/social-analyzer/blob/main/info) [![Generic badge](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/qeeqbox/social-analyzer/main/info&label=build&query=$.build&colorB=green&style=flat-square)](https://github.com/qeeqbox/social-analyzer/blob/main/info) [![Generic badge](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/qeeqbox/social-analyzer/main/info&label=test&query=$.test&colorB=green&style=flat-square)](https://github.com/qeeqbox/social-analyzer/blob/main/info) [![Generic badge](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/qeeqbox/social-analyzer/main/info&label=verified%20sites&query=$.websites&colorB=blue&style=flat-square)](https://github.com/qeeqbox/social-analyzer/blob/main/info) [![Generic badge](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/qeeqbox/social-analyzer/main/info&label=verified%20detections&query=$.detections&colorB=blue&style=flat-square)](https://github.com/qeeqbox/social-analyzer/blob/main/info) [![Generic badge](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/qeeqbox/social-analyzer/main/info&label=special%20detections&query=$.special&colorB=blue&style=flat-square)](https://github.com/qeeqbox/social-analyzer/blob/main/info) [![Generic badge](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/qeeqbox/social-analyzer/main/info&label=awaiting%20verification&query=$.awaiting_verification&colorB=orange&style=flat-square)](https://github.com/qeeqbox/social-analyzer/blob/main/info) [![Generic badge](https://img.shields.io/static/v1?label=%F0%9F%91%8D&message=!&color=yellow&style=flat-square)](https://github.com/qeeqbox/social-analyzer/stargazers)
 
-API and Web App for analyzing & finding a person profile across +300 social media websites. It includes different string analysis and detection modules, you can choose which combination of modules to use during the investigation. 
+API and Web App for analyzing & finding a person profile across +300 social media websites. It includes different string analysis and detection modules, you can choose which combination of modules to use during the investigation.
 
 The detection modules utilize a rating mechanism based on different detection techniques, which produces a rate value that starts from 0 to 100 (No-Maybe-Yes)
 
@@ -12,6 +12,7 @@ This project could help in investigating profiles related to suspicious or malic
 **Pull Requests are welcomed!**
 
 ## Updates
+- Added more customization to the CLI python version (It's a lot easier to integrate using the JSON output switch) 👏
 - Added Custom Search Query 👏
 - Added similarity checking to FindOrigins 👏
 - Added Proxy option for FindUserProfilesFast & FindUserProfilesSlow 👏
@@ -99,10 +100,10 @@ sudo apt-get update
 sudo apt-get install -y firefox-esr tesseract-ocr git
 git clone https://github.com/qeeqbox/social-analyzer.git
 cd social-analyzer
-pip3 install lxml BeautifulSoup4 tld
+pip3 install lxml BeautifulSoup4 tld pygments
 # If you want to list all websites use python3 app.py -c -l
 # Remember the following runs as FindUserProfilesFast
-python3 app.py -c -m "fast" -u "username" -w "youtube pinterest tumblr"
+python3 app.py --cli --mode "fast" --username "johndoe" --websites "youtube pinterest tumblr" --output "pretty"
 ```
 
 ## Install and run as web app with a grid (docker-compose)
--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 """
 //  -------------------------------------------------------------
 //  author        Giga
-//  project       qeeqbox/social-analyzer 
+//  project       qeeqbox/social-analyzer
 //  email         gigaqeeq@gmail.com
 //  description   app.py (CLI)
 //  licensee      AGPL-3.0
@@ -11,10 +11,9 @@
 //  -------------------------------------------------------------
 """
 
-print("[!] Detections are updated every often, make sure to get the most updated ones")
-
 from logging import getLogger, DEBUG, StreamHandler, Formatter
 from logging.handlers import RotatingFileHandler
+from contextlib import contextmanager
 from sys import stdout
 from os import path, makedirs
 from requests import get
@@ -26,12 +25,26 @@ from uuid import uuid4
 from tld import get_fld
 from functools import wraps
 from bs4 import BeautifulSoup
+from json import dumps
 from queue import Queue
+from pygments import highlight, lexers, formatters
+from re import sub as resub
 
 QUEUE = Queue()
 USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0"
 PARSED_SITES = []
 LOG = getLogger("social-analyzer")
+
+@contextmanager
+def ignore_excpetion(*exceptions):
+    '''
+    catch excpetion
+    '''
+    try:
+        yield
+    except exceptions as error:
+        #print("{} {} {}".format(datetime.utcnow(), EXCLAMATION_MARK, error))
+        pass
 
 def check_errors(on_off=None):
 	def decorator(func):
@@ -47,9 +60,6 @@ def check_errors(on_off=None):
 				return func(*args, **kwargs)
 		return wrapper
 	return decorator
-
-def msg(name=None):
-	return '''app.py -c -m "fast" -u "johndoe" -w "youtube tiktok'''
 
 @check_errors(True)
 def setup_logger(uuid=None,file=False):
@@ -81,18 +91,19 @@ def list_all_websites():
 			LOG.info(x)
 
 @check_errors(True)
-def find_username_normal(req):
+def find_username_normal(req, options):
 
 	@check_errors(True)
-	def fetch_url(queue, site):
-		LOG.info("[Checking] "+ get_fld(site["url"]))
+	def fetch_url(queue, site, options):
+		if options.output != "json":
+			LOG.info("[Checking] "+ get_fld(site["url"]))
 		timeout = site["timeout"] if site["timeout"] != 0 else 10
 		implicit = site["implicit"] if site["implicit"] != 0 else 5
 		detections_count = 0;
 		source = get(site["url"].replace("{username}", req["body"]["string"]), timeout=(implicit, timeout)).text
 		text_only = "unavailable";
 		title = "unavailable";
-		temp_profile = {"found": 0,"image": "","link": "","rate": "","title": "","text": ""};
+		temp_profile = {"found": 0,"link": "","rate": "","title": "","text": ""};
 		for detection in site["detections"]:
 			temp_found = "false";
 			if detection["type"] == "normal" and source != "" and detection["return"] == "true":
@@ -103,10 +114,20 @@ def find_username_normal(req):
 					temp_profile['found'] += 1
 
 		if temp_profile["found"] > 0 and detections_count != 0:
-			temp_profile["text"] = "".join(BeautifulSoup(source, "lxml").findAll(text=True))
+			with ignore_excpetion():
+				soup = BeautifulSoup(source, 'html.parser')
+				[tag.extract() for tag in soup(['head', 'title','style', 'script', '[document]'])]
+				temp_profile["text"] = soup.getText()
+				temp_profile["text"] = resub("\s\s+", " ", temp_profile["text"])
+				temp_profile["text"] = temp_profile["text"].replace("\n", "").replace("\t", "").replace("\r", "").strip()
+			with ignore_excpetion():
+				temp_profile["title"] = BeautifulSoup(source, 'html.parser').title.string
+				temp_profile["title"] = resub("\s\s+", " ", temp_profile["title"])
+				temp_profile["title"] = temp_profile["title"].replace("\n", "").replace("\t", "").replace("\r", "").strip()
 			if temp_profile["text"] == "":
 				temp_profile["text"] = "unavailable"
-			temp_profile["title"] = "".join(BeautifulSoup(title, "lxml").findAll(text=True))
+			if temp_profile["title"] == "":
+				temp_profile["title"] = "unavailable"
 			temp_profile["rate"] = "%" + str(round(((temp_profile["found"] / detections_count) * 100), 2))
 			temp_profile["link"] = site["url"].replace("{username}", req["body"]["string"]);
 			copy_temp_profile = temp_profile.copy()
@@ -114,50 +135,76 @@ def find_username_normal(req):
 		else:
 			queue.put(None)
 
-	threads = [Thread(target=fetch_url, args=(QUEUE,site)) for site in PARSED_SITES if site["selected"] == "true"]
+	threads = [Thread(target=fetch_url, args=(QUEUE,site,options)) for site in PARSED_SITES if site["selected"] == "true"]
 	for thread in threads:
 		thread.start()
 	for thread in threads:
 		thread.join()
 
 @check_errors(True)
-def check_user_cli(username, websites):
+def check_user_cli(parsed):
 	temp_found = []
-	req = {"body": {"uuid": str(uuid4()),"string": username,"option": "FindUserProfilesFast"}}
+	temp_keys = {"found": 0,"link": "","rate": "","title": "","text": ""};
+	req = {"body": {"uuid": str(uuid4()),"string": parsed.username,"option": "FindUserProfilesFast"}}
 	setup_logger(req["body"]["uuid"],True)
 	for site in PARSED_SITES:
-		for temp in websites.split(" "):
+		for temp in parsed.websites.split(" "):
 			if temp in site["url"]:
 				site["selected"] = "true"
-	find_username_normal(req)
+	find_username_normal(req, parsed)
 	while not QUEUE.empty():
 		items = QUEUE.get()
 		if items != None:
 			for item in items:
 				if item != None:
-					del item['text']
-					del item['title']
-					del item['image']
+					if parsed.options == "" or parsed.options == None:
+						del item['text']
+					else:
+						for key in temp_keys:
+							if key not in parsed.options:
+								with ignore_excpetion():
+									del item[key]
 					temp_found.append(item)
 	if len(temp_found) == 0:
-		LOG.info('User does not exist (try FindUserProfilesSlow or FindUserProfilesSpecial) in the web app version')
+		if parsed.output != "json":
+			LOG.info('User does not exist (try FindUserProfilesSlow or FindUserProfilesSpecial) in the web app version')
 	else:
-		for item in temp_found:
-			LOG.info(item)
+		if parsed.output == "json":
+			print(dumps(temp_found, sort_keys=True, indent=None))
+		elif parsed.output == "pretty":
+			LOG.info('[Total Profiles Found] {}\n'.format(len(temp_found)))
+			for item in temp_found:
+				LOG.info(highlight(dumps(item, sort_keys=True, indent=4), lexers.JsonLexer(), formatters.TerminalFormatter()))
+		else:
+			LOG.info('[Total Profiles Found ] {}\n'.format(len(temp_found)))
+			for item in temp_found:
+				LOG.info(item)
+
+def msg(name=None):
+	return '''app.py --cli --mode "fast" --username "johndoe" --websites "youtube tiktok"'''
 
 PARSED_SITES = init_websites()
-parser = ArgumentParser(description="Qeeqbox/social-analyzer - API and Web App for analyzing & finding a person profile across 300+ social media websites (Detections are updated regularly)",usage=msg())
-parser.add_argument("-c","-cli",action="store_true", help="Turn this CLI on")
-parser.add_argument("-u","-username", help="E.g. johndoe, john_doe or johndoe9999", metavar="")
-parser.add_argument("-w","-website", help="Website or websites separated by space E.g. youtube, tiktok or tumblr", metavar="")
-parser.add_argument("-l","-list", action="store_true",  help="List all available websites")
-parser.add_argument("-o","-output", help="This option will be implemented..", metavar="")
-parser.add_argument("-m","-mode", help="Analysis mode E.g.fast -> FindUserProfilesFast\nslow -> FindUserProfilesSlow\nspecial -> FindUserProfilesSpecial", metavar="")
-parsed = parser.parse_args()
-if parsed.c:
-	if parsed.l:
+arg_parser = ArgumentParser(description="Qeeqbox/social-analyzer - API and Web App for analyzing & finding a person profile across 300+ social media websites (Detections are updated regularly)",usage=msg())
+arg_parser._action_groups.pop()
+arg_parser_required = arg_parser.add_argument_group('Required Arguments')
+arg_parser_required.add_argument("--cli",action="store_true", help="Turn this CLI on", required=True)
+arg_parser_required.add_argument("--username", help="E.g. johndoe, john_doe or johndoe9999", metavar="", required=True)
+arg_parser_required.add_argument("--websites", help="Website or websites separated by space E.g. youtube, tiktok or tumblr", metavar="" ,required=True)
+arg_parser_required.add_argument("--mode", help="Analysis mode E.g.fast -> FindUserProfilesFast, slow -> FindUserProfilesSlow or special -> FindUserProfilesSpecial", metavar="", required=True)
+arg_parser_optional = arg_parser.add_argument_group('Optional Arguments')
+arg_parser_optional.add_argument("--output", help="Show the output in the following format: json -> json output for integration or pretty -> prettify the output", metavar="", default="")
+arg_parser_optional.add_argument("--options", help="Show the following when a profile is found: link, rate, title or text", metavar="", default="")
+arg_parser_list = arg_parser.add_argument_group('Listing websites & detections')
+arg_parser_list.add_argument("--list", action="store_true",  help="List all available websites")
+parsed = arg_parser.parse_args()
+
+if parsed.output != "json":
+	print("[!] Detections are updated every often, make sure to get the most updated ones")
+
+if parsed.cli:
+	if parsed.list:
 		setup_logger()
 		list_all_websites()
-	elif parsed.m == "fast":
-		if parsed.u != "" and parsed.w != "":
-			check_user_cli(parsed.u,parsed.w)
+	elif parsed.mode == "fast":
+		if parsed.username != "" and parsed.websites != "":
+			check_user_cli(parsed)
