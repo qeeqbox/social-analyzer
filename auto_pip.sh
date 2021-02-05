42a663d8261f4ff018791308e34fac47e421db6b
G <52905881+giga-a@users.noreply.github.com>
2021-02-05 12:04:04 -0800

    Add files via upload

---
+++ auto_pip.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Auto pip Package Creator - QeeqBox"
+echo "[x] Deleting pip-social-analyzer"
+[[ -d pip-social-analyzer ]] && rm -r pip-social-analyzer
+echo "[x] Making pip-social-analyzer"
+mkdir -p pip-social-analyzer/social-analyzer
+echo "[x] Copying app.py, setup.py, README.rst & README.rst"
+cp app.py pip-social-analyzer/__main__.py
+cp setup.py pip-social-analyzer/setup.py
+cp README.rst pip-social-analyzer/README.rst
+echo "[x] Copying data folder"
+cp -r data pip-social-analyzer/
+cd pip-social-analyzer
+echo "[x] Checking setup.py"
+python3 setup.py check -r -s
+echo "[x] Creating pypi Package"
+
+python3 setup.py sdist bdist_wheel 2>stderr.log 1>stdout.log
+
+ if grep -q "error:" stderr.log
+	then
+		echo "[x] Creating pypi failed.."
+		cat stderr.log
+	else
+		echo "[x] pypi Package was created successfully"
+		read -p "Do you want to upload? (y/n)?" USER_INPUT
+		if [ "$USER_INPUT" = "y" ]; then
+			echo "[x] Uploading pypi Package"
+			twine upload dist/*
+		fi
+ fi
+
+echo "[x] Cleaning.."
+[[ -d pip-social-analyzer ]] && rm -r pip-social-analyzer
+echo "[x] Done"
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='social-analyzer',
+    author='QeeqBox',
+    author_email='gigaqeeq@gmail.com',
+    description="API, CLI & Web App for analyzing & finding a person's profile across 300+ social media websites (Detections are updated regularly)",
+    long_description=long_description,
+    version='0.4',
+    license='AGPL-3.0',
+    url='https://github.com/qeeqbox/social-analyzer',
+    packages=['socialanalyzer'],
+    include_package_data=True,
+    entry_points={'console_scripts': ['social_analyzer = social_analyzer.__main__:main']},
+    install_requires=['BeautifulSoup4', 'tld', 'pygments', 'langdetect', 'requests'],
+    package_data={'social-analyzer': ['data/*']},
+    python_requires='>=3',
+    )
+
