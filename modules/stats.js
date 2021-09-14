a072f81df6492905ea15efe34400f110ba114376
Giga <52905881+giga-a@users.noreply.github.com>
2021-09-14 16:12:15 -0700

    [auto-b] added stats section - ref ce1fc3d68e9b

--- modules/stats.js
+++ modules/stats.js
@@ -46,17 +46,66 @@ async function get_stats_by_value (data, value) {
   return temp_array
 }
 
-async function get_stats (data) {
+async function get_metadata(data, type){
+  const temp_array = []
+  try {
+    const temp_filtered = data.filter(item => item.status === type)
+    await temp_filtered.forEach(site => {
+      if ('metadata' in site) {
+        if (site.metadata !== 'unavailable' && site.metadata.length > 0) {
+          site.metadata.forEach(meta => {
+            if ('content' in meta) {
+              let temp_content = ''
+              if ('name' in meta) {
+                temp_content = meta.content
+              } else if ('itemprop' in meta) {
+                temp_content = meta.content
+              } else if ('property' in meta) {
+                temp_content = meta.content
+              }
+
+              if (temp_content != ''){
+                const entry = temp_array.find( x => x[1] === temp_content);
+                if (entry) {
+                    ++entry[0];
+                } else {
+                    temp_array.push([1,temp_content]);
+                }
+              }
+            }
+          })
+        }
+      }
+    })
+  }
+  catch(error){
+    helper.verbose && console.log(error)
+  }
+
+  if (temp_array.length >0) {
+    temp_array.sort(function(a,b) {return b[0]-a[0]})
+  }
+
+  return temp_array
+}
+
+async function get_stats (req, data) {
   let categories = {}
   let countries = {}
+  let metadata = []
   try {
-    categories = await get_stats_by_value(data, 'type')
-    countries = await get_stats_by_value(data, 'country')
+    if (req.body.option.includes('CategoriesStats')) {
+      categories = await get_stats_by_value(data, 'type')
+      countries = await get_stats_by_value(data, 'country')
+    }
+    if (req.body.option.includes('ExtractMetadata') && req.body.option.includes('MetadataStats')){
+      metadata = await get_metadata(data, 'good')
+    }
   } catch (error) {
     helper.verbose && console.log(error)
   }
 
-  return { categories: categories, countries: countries }
+  return { categories: categories, countries: countries, metadata: metadata}
 }
 
 module.exports = {
