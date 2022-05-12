55a38fe6f0a9aa54c7e531bb7690950b5b51dfa4
Giga <52905881+giga-a@users.noreply.github.com>
2022-05-12 13:10:52 -0700

    Update helper.js

--- modules/helper.js
+++ modules/helper.js
@@ -54,7 +54,7 @@ const detected_websites = {
 
 const header_options = {
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0'
   }
 }
 
